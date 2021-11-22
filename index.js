c8d3b6c21eec00f5c1302484d7dac8f46e29bee0
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-11-21 19:01:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,16 +15,17 @@ export default class panelDashboard extends Plugin {
   start () {
     FluxDispatcher.subscribe("CHANNEL_SELECT", this.channelSwitch = ({ guildId, channelId, messageId }) => this.settings.set("oldChannel", [guildId, channelId, messageId]))
     patch("AccountConnected", getOwnerInstance(ele).__proto__, "render", (_, res) => {
-      res.props.children[res.props.children.length - 1].props.children.unshift(<PanelButton 
-        icon={() => <Icon name="Vizality" size={20} />}
-        tooltipText="Dashboard"
-        onClick={() => this.toggle.apply(this)}
-        onContextMenu={(evt) => contextMenu.openContextMenu(evt, () => <SettingsContextMenu />)}
-      />)
+      res.props.children[res.props.children.length - 1].props.children.unshift(
+        <PanelButton 
+          icon={() => ( <Icon name="Vizality" size={20} /> )}
+          tooltipText="Dashboard"
+          onClick={() => this.toggle.apply(this)}
+          onContextMenu={(evt) => contextMenu.openContextMenu(evt, () => ( <SettingsContextMenu /> ))}
+        />
+      )
     })
   }
   stop () { FluxDispatcher.unsubscribe("CHANNEL_SELECT", this.channelSwitch) }
-  // Simple toggle between dashboard and the channel the user was in before
   toggle () {
     const current = vizality.api.routes.getLocation()
     if (current.pathname.startsWith("/vizality")) transitionToGuild(...this.settings.get("oldChannel"))
